c1f6525855bcba8e1ca0cdb86789b350c12b8657
Phantom <email@provider.com>
2019-07-19 17:59:30 +0200

    php lernen

---
+++ xpath/displayXml.php
@@ -0,0 +1,15 @@
+ <!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Titel</title>
+  </head>
+  <body>
+<?php
+$fileName = $_FILES['xmlFile']['name'];
+
+echo $fileName;
+?>
+  </body>
+</html>
