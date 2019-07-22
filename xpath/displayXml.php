9e8f94ba66e44d1913ca49b94e256ad73330fddc
Phantom <email@provider.com>
2019-07-22 16:07:52 +0200

    Implemented of first and lastnames

--- xpath/displayXml.php
+++ xpath/displayXml.php
@@ -11,17 +11,102 @@ function endsWith($haystack, $needle) {
     return substr_compare($haystack, $needle, -strlen($needle)) === 0;
 }
 
+
+
+
+
+
+function extractFirstName($i, $xpath) {
+	$query  = "/*[local-name()='rechnungen' and namespace-uri()='http://padinfo.de/ns/pad']/*[local-name()='rechnung' and namespace-uri()='http://padinfo.de/ns/pad'][" . $i . "]/*[local-name()='rechnungsempfaenger' and namespace-uri()='http://padinfo.de/ns/pad'][1]/*[local-name()='person' and namespace-uri()='http://padinfo.de/ns/pad'][1]/*[local-name()='vorname' and namespace-uri()='http://padinfo.de/ns/pad'][1]";
+	$nodeList = $xpath->evaluate($query);
+	
+	return $nodeList->item(0)->nodeValue ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function extractLastName($i, $xpath) {
+	$query  = "/*[local-name()='rechnungen' and namespace-uri()='http://padinfo.de/ns/pad']/*[local-name()='rechnung' and namespace-uri()='http://padinfo.de/ns/pad'][" . $i . "]/*[local-name()='rechnungsempfaenger' and namespace-uri()='http://padinfo.de/ns/pad'][1]/*[local-name()='person' and namespace-uri()='http://padinfo.de/ns/pad'][1]/*[local-name()='name' and namespace-uri()='http://padinfo.de/ns/pad'][1]";
+	$nodeList = $xpath->evaluate($query);
+	
+	return $nodeList->item(0)->nodeValue ;
+}
+
 $fileName = $_FILES['xmlFile']['name'];
 $endsWithXml = endsWith($fileName,".xml");
 
-if ($endsWithXml){
-  echo "Dateiname endet mit XML";
-} else {
-  echo "Dateiname endet nicht mit XML";
+if ($_FILES["xmlFile"]["error"] > 0)
+{
+  echo "Error: " . $_FILES["xmlFile"]["error"] . "<br />";
 }
 
-echo "result of endsWith:" . endsWith($fileName,".xml");
+
+if (!$endsWithXml){
+  echo "Dateiname endet nicht mit XML";
+  die();
+}
+
+//echo "Dateiname endet mit XML";
+
+$uploadedFileName = 'upload/'.$_FILES['xmlFile']['name'];
+$uploadResult = move_uploaded_file($_FILES['xmlFile']['tmp_name'], $uploadedFileName);
+
+echo "Ergebnis des Hochladens:" . $uploadResult . "<br>";
+
+$tmpFileName = $_FILES['xmlFile']['tmp_name'];
+
+echo "Temporäre Datei:" . $tmpFileName;
 // echo $fileName;
+
+ $myfile = fopen($uploadedFileName, "r") or die("Unable to open file!");
+//echo "<pre>" . fread($myfile,filesize($uploadedFileName)) . "</pre>";
+
+$xmlText = fread($myfile,filesize($uploadedFileName));
+
+fclose($myfile);
+
+$doc = new DOMDocument;
+
+// We don't want to bother with white spaces
+$doc->preserveWhiteSpace = false;
+$doc->loadXML($xmlText);
+
+$xpath = new DOMXPath($doc);
+
+$query = "count(/*[local-name()='rechnungen' and namespace-uri()='http://padinfo.de/ns/pad']/*[local-name()='rechnung' and namespace-uri()='http://padinfo.de/ns/pad'])";
+
+//$xpath->evaluate($query);
+$RechungAnzahl = $xpath->evaluate($query);
+
+for ($i = 1; $i <= $RechungAnzahl ; $i++) {
+    echo "<h3>Rechnung Nummer:".$i."</h3>";
+	$name = extractLastName($i, $xpath);
+	$firstName = extractFirstName($i, $xpath);
+	echo "Name:". $name."<br>";
+	echo "Vorname:".$firstName;
+}
+
+
+
+
 ?>
   </body>
 </html>
